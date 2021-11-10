9f9352797d3e7e584d65d4f44f3c4f45fb980f58
RenzoOliveira1784 <renzorib1218@gmail.com>
2021-11-10 20:35:12 -0300

    try_main_1

---
+++ renzo_part.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
+
+int main(){
+    int n_algoritmo;
+    int n_arquivo;
+    printf("Olá usuário, aqui estão os algotimos que podem ser usados: ");
+    printf("\n1 - Insertion Sort\n2 - Selction Sort\n3 - Bubble Sort\n4 - Heap Sort\n");
+    scanf("Digite o número correspondente ao algoritmo que você deseja que seja usado:\n%d", &n_algoritmo);
+    printf("\nDigite o número correspondente ao tamanho de arquivo que você deseja que seja usado: ");
+    scanf("\n1000\n5000\n10000\n20000\n50000\n75000\n100000: \n%d", &n_arquivo);
+    
+
+
+    
+    
+    
+    //first read the archives and put them in a array
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
