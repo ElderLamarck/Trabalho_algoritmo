1df72e4aa6657d2f360d2bc00749706d6e649723
ElderLamarck <elgr@cesar.school>
2021-11-10 21:46:32 -0300

    FileOpener

--- elgr_rro_sort.c
+++ elgr_rro_sort.c
@@ -3,8 +3,38 @@
 
 int fileOpener(int n_arquivo){
     int values[n_arquivo];
-    char number = n_arquivo;
-    printf("%c ahaha", number);
+    int value;
+    int i = 0;
+    FILE *file;
+
+    if (n_arquivo == 1000){
+        file = fopen("arquivos/1000.txt", "r");
+    } else if (n_arquivo == 5000){
+        file = fopen("arquivos/5000.txt", "r");
+    } 
+    else if (n_arquivo == 10000){
+        file = fopen("arquivos/10000.txt", "r");
+    } 
+    else if (n_arquivo == 20000){
+        file = fopen("arquivos/20000.txt", "r");
+    } 
+    else if (n_arquivo == 75000){
+        file = fopen("arquivos/75000.txt", "r");
+    } 
+    else if (n_arquivo == 100000){
+        file = fopen("arquivos/100000.txt", "r");
+    }
+
+    if(file == NULL){
+        printf("nao foi possivel abrir o arquivo\n");
+    } else {
+        while((fscanf(file,"%d", &value))!= EOF){
+            printf("%d\n", value);
+            values[i] = value;
+            i++;
+        }
+    }
+    
 }
 
 void insertionSort(int n_arquivo, int values[]){
