9b859a08cf0e4d28fadf722c0750a0fd16e8f4f8
RenzoOliveira1784 <renzorib1218@gmail.com>
2021-11-10 22:13:17 -0300

    mundança da funcao pro main de verdade

--- elgr_rro_sort.c
+++ elgr_rro_sort.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include <time.h>
 
-int* fileOpener(int n_arquivo){
-    int values[n_arquivo];
-    int* apontando = malloc(n_arquivo * sizeof(int));
-    
+
+void insertionSort(int n_arquivo, int values[]){
+    int step;
+    int temp;
+    int i;
+
+    for (step = 1; step < n_arquivo; step++){
+    temp = values[step];
+    i = step - 1;
+
+    while (temp < values[i] && i >= 0) {
+      values[i + 1] = values[i];
+      i--;
+    }
+    values[i + 1] = temp;
+  }
+}
+
+
+int main(void){
+    int n_algoritmo;
+    int n_arquivo;
+    printf("Olá usuário, aqui estão os algotimos que podem ser usados: ");
+    printf("\n1 - Insertion Sort\n2 - Selction Sort\n3 - Bubble Sort\n4 - Heap Sort\n");
+    printf("Digite o numero correspondente ao algoritmo que voce deseja que seja usado:\n");
+    scanf("%d", &n_algoritmo);
+
+    printf("\nDigite o número correspondente ao tamanho de arquivo que você deseja que seja usado: ");
+    printf("\n1000\n5000\n10000\n20000\n50000\n75000\n100000:\n");
+    scanf("%d", &n_arquivo);
+
+    int values[n_arquivo]; 
     int value;
     int i = 0;
     FILE *file;
@@ -30,41 +58,14 @@ int* fileOpener(int n_arquivo){
     if(file == NULL){
         printf("nao foi possivel abrir o arquivo\n");
     } else {
-        while((fscanf(file,"%d", value))!= EOF){
+        while((fscanf(file,"%d", &value))!= EOF){
             values[i] = value;
             i++;
         }
         fclose(file);
 
     }
-    return apontando;    
-}
 
-void insertionSort(int n_arquivo, int values[]){
-    int step;
-    int temp;
-    int i;
 
-    for (step = 1; step < n_arquivo; step++){
-    temp = values[step];
-    i = step - 1;
-
-    while (temp < values[i] && i >= 0) {
-      values[i + 1] = values[i];
-      i--;
-    }
-    values[i + 1] = temp;
-  }
-}
-
-int main(void){
-    int n = 1000, i;
-    int* retorno = fileOpener(n);
-
-    for (i = 0; i < n; i++){
-        printf("%d ", retorno[i]);
-    }
-    
     return 0;
-    //lembrar de dar o free na lista dinamicamente alocada
 }
